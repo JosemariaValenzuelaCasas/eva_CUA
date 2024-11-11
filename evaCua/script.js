4f1862222996fd05626b1573953c8eab02fa0fc3
JosemariaValenzuelaCasas <jose.valenzuela.casas@vallegrande.edu.pe>
2024-11-11 16:00:10 -0500

    Update

--- evaCua/script.js
+++ evaCua/script.js
@@ -1,12 +1,31 @@
-const logo=document.getElementById("logo");
-const barra=document.querySelector(".barra-lateral")
-const spans =document.querySelectorAll("span")
-const main =document.querySelector("main")
+const cloud = document.getElementById("cloud");
+const barraLateral = document.querySelector(".barra-lateral")
+const spans = document.querySelectorAll("span");
+const modoOscuro = document.getElementById("modo-oscuro")
+const palanca = document.querySelector(".switch")
+const circulo = document.querySelector(".circulo");
 
-logo.addEventListener("click",()=>{
-    barra.classList.toggle("min-barra")
-    main.classList.toggle("oculto")
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+})
+cloud.addEventListener("click",()=>{
+    barraLateral.classList.toggle("mini-barra-lateral");
+    modoOscuro.classList.toggle("mini-modo-oscuro");
+
     spans.forEach((spans)=>{
         spans.classList.toggle("oculto")
+        
     })
+    
 })
+
+function scrollToSection(event, id) {
+    event.preventDefault();  // Evita el comportamiento predeterminado del enlace
+    const section = document.getElementById(id);
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth"  // Desplazamiento suave
+    });
+}
